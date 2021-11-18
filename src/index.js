f932cf4f4065e743df596bdba1d89dbf088e05ff
solomirrors <solomirrors@icloud.com>
2021-11-18 19:26:44 +0100

    Using useEffect to load data

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, {useContext, useEffect, useState} from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-    const [value, setValue] = useState(0);
+    const [value, setValue] = useState(1);
     const [visible, setVisible] = useState(true);
 
     if (visible) {
@@ -18,11 +18,29 @@ const App = () => {
                 >
                     Hide
                 </button>
-                <Notification/>
+                <PlanetInfo id={value}/>
             </div>
         )
     }
     else return <div>Hide Closed</div>
+}
+
+const PlanetInfo = ( {id} ) => {
+    const [planetName, setPlanetName] = useState(null);
+
+    useEffect(() => {
+        let cancelled = false;
+        fetch(`https://swapi.dev/api/planets/${id}`)
+            .then(res => res.json())
+            .then(data => !cancelled && setPlanetName(data.name));
+        return () => cancelled = true;
+    }, [id]);
+
+    return (
+        <div>
+            <h1>{id} - {planetName}</h1>
+        </div>
+    )
 }
 
 const HookCounter = ({value}) => {
@@ -31,7 +49,7 @@ const HookCounter = ({value}) => {
         return () => {
             console.log('EffectUnMount')
         }
-    },[])
+    })
 
     return <p>{value}</p>
 }
@@ -52,7 +70,6 @@ const Notification = () => {
         </div>
     )
 }
-
 
 ReactDOM.render(
     <React.StrictMode>
